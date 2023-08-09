24f990255f263403dae931c4657ffb6744f3ec18
Vargos98 <127929058+Vargos98@users.noreply.github.com>
2023-08-09 12:26:29 +0530

    Added few Functionalities

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,22 @@
 import './navbar.css'
+import Logo from './Logo.png'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className="container">
+        <div className="left">
+          <img className="logo"src={Logo} alt="logo"/>
+        </div>
+        <div className="right">
+          <ul className='list__items'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact us</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
